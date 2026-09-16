f4f145ea3db73492ce78cf8b8b0cb77f338c14a9
GruzdevAV <9516688010@mail.ru>
2026-09-16 20:54:02 +0300

    Теперь введённые некорректные данные не вызывают исключений

--- Lab1/src/lab1/Hero.java
+++ Lab1/src/lab1/Hero.java
@@ -2,7 +2,7 @@ package lab1;
 
 
 public class Hero {
-	public ACMovement wayOfMovement;
+	public ACMovement wayOfMovement = new MoveByFeet();
 	public Point location = new Point(0,0);
 	public void move(Point destination) {
 		wayOfMovement.move(location, destination);
--- Lab1/src/lab1/MainClass.java
+++ Lab1/src/lab1/MainClass.java
@@ -8,28 +8,41 @@ public class MainClass {
 		Scanner scanner = new Scanner(System.in);
 		boolean loop = true;
 		while(loop) {
-			System.out.println(String.format("Hero at point (%.2f; %.2f)", hero.location.x, hero.location.y));
+			System.out.println(String.format("\nHero is at point (%.2f; %.2f).", hero.location.x, hero.location.y));
+			System.out.println("Movement method: " + hero.wayOfMovement.getName());
 			System.out.println("Choose movement method:\n\t1 - walk\n\t2 - horse\n\t3 - plane\n\n\t0 - break");
-			switch(scanner.nextInt()) {
-			case 1:
-				hero.wayOfMovement = new MoveByFeet();
-				break;
-			case 2:
-				hero.wayOfMovement = new MoveByHorse();
-				break;
-			case 3:
-				hero.wayOfMovement = new MoveByPlane();
-				break;
-			case 0:
-				loop = false;
-				continue;
-			default:
-				System.out.println("Previous remains");
-				break;
+			if (!scanner.hasNextInt()) {
+				scanner.next();
+				System.out.println("Previous movement method remains.");
 			}
-			System.out.print("Enter next x -> ");
+			else {
+				switch(scanner.nextInt()) {
+				case 1:
+					hero.wayOfMovement = new MoveByFeet();
+					break;
+				case 2:
+					hero.wayOfMovement = new MoveByHorse();
+					break;
+				case 3:
+					hero.wayOfMovement = new MoveByPlane();
+					break;
+				case 0:
+					loop = false;
+					continue;
+				default:
+					System.out.println("Previous movement method remains.");
+					break;
+				}
+			}
+			do {
+				System.out.print("Enter next x -> ");
+			}
+			while (!scanner.hasNextDouble() && scanner.next().contains(""));
 			double x = scanner.nextDouble();
-			System.out.print("Enter next y -> ");
+			do {
+				System.out.print("Enter next y -> ");
+			}
+			while (!scanner.hasNextDouble() && scanner.next().contains(""));
 			double y = scanner.nextDouble();
 			hero.move(new Point(x, y));
 		}
